0da60349aedef907c98895da1ad2a9e6ae9d149c
benjholla <benjholla@gmail.com>
2014-09-08 18:34:59 -0500

    added PBKDF2 hashing implementation

--- LoginSideChannels/src/com/example/loginsidechannels/AuthUtils.java
+++ LoginSideChannels/src/com/example/loginsidechannels/AuthUtils.java
@@ -19,9 +19,29 @@ public class AuthUtils {
 		return salt;
 	}
 	
+	// Using a minimum of 1000 hash rounds as recommended by https://en.wikipedia.org/wiki/PBKDF2
+	private static int PBKDF2_ROUNDS = 1000;
+	
 	public static String hash(String password, String salt) throws NoSuchAlgorithmException {
 		String hashedPassword = "";
 		byte[] buffer = (password + salt).getBytes();
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		md.update(buffer);
+		byte[] digest = md.digest();
+		for (int i=0; i<digest.length; i++) {
+			hashedPassword += Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1).toUpperCase();
+		}
+		
+		for(int round=0; round<PBKDF2_ROUNDS; round++){
+			hashedPassword = pbkdf2(hashedPassword);
+		}
+		
+		return hashedPassword;
+	}
+	
+	public static String pbkdf2(String password) throws NoSuchAlgorithmException {
+		String hashedPassword = "";
+		byte[] buffer = password.getBytes();
 		MessageDigest md = MessageDigest.getInstance("SHA-512");
 		md.update(buffer);
 		byte[] digest = md.digest();
